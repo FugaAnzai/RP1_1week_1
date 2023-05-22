b5bc1d4668258e6a4c0e083c058adfa95854b187
KeroppiKun <fuga0809ace@gmail.com>
2023-05-23 01:52:10 +0900

    粉を前に押せるように

--- Assets/Teacher/PowderScript.cs
+++ Assets/Teacher/PowderScript.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PowderScript : MonoBehaviour
 {
-
-
-
     //生成されてから経った時間
     private float livingTime = 0;
     //プレイヤーに当たるか。これが無きゃ生成されて即合体!!
     private const float kCanHitPlayerTime = 0.1f;
     private bool canHitPlayer = false;
-    
+    private bool isMove = false;
+
+    private float moveT = 0;
+
+    private Vector3 start = Vector3.zero;
+    private Vector3 end = Vector3.zero;
 
     public bool GetCanHitPlayer()
     {
@@ -24,9 +28,25 @@ public class PowderScript : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        UnityEngine.Debug.Log("powderHit");
+
+        if (collision.tag == "Player" && isMove == false)
+        {
+            UnityEngine.Debug.Log("powderHit");
+            start = this.transform.position;
+            end = this.transform.position + new Vector3(1, 0, 0);
+            moveT = 0;
+            isMove = true;
+
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
+
         if (livingTime >= kCanHitPlayerTime)
         {
             canHitPlayer = true;
@@ -34,6 +54,17 @@ public class PowderScript : MonoBehaviour
 
         livingTime += Time.deltaTime;
 
+        if(isMove)
+        {
+            moveT += 0.01f;
+            this.transform.position = Vector3.Lerp(start, end, moveT);
+
+            if(moveT > 1.0f)
+            {
+                isMove = false;
+            }
+
+        }
 
     }
 
