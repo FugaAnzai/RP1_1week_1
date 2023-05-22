9170dce5a2aa5d4f810794770178b622b48a03c5
瑞生 志賀 <nekuranoneku@gmail.com>
2023-05-23 00:42:42 +0900

    タイマー追加

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class GameManager : MonoBehaviour
 {
-    
 
-    
+    public float timer = 0.0f;
+    public float timeLimit = 60.0f;
+
+    public GameObject timerObject;
+
+    private Image timerImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = timeLimit;
+        timerImage = timerObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        
+
+        if(timer <= 0.0f)
+        {
+            timer = 0.0f;
+        }
+
+        timerImage.fillAmount = timer / timeLimit;
+
+        if (timer > 0.0f)
+        {
+            timer -= Time.deltaTime;
+        }
     }
 }
